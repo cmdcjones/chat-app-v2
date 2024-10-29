117b32e4d522cf87aed3301e51db56e9d8dddd51
Dominique Chin-Jones <djones@rapptrlabs.com>
2024-10-28 22:35:40 -0400

    create Message model; migration

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    # room = models.ForeignKey('Room', related_name='messages', on_delete=models.CASCADE)
+    room_name = models.CharField(max_length=255)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    sender = models.CharField(max_length=255)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.sender}: {self.content} ({self.timestamp})"
+
+# class Room(models.Model):
+#     name = models.TextField()
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
